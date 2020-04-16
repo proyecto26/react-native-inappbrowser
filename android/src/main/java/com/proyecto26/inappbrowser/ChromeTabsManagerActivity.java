ce19eb9adb138494fb3ac4e6e599a398dfe9d576
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2020-04-16 10:52:25 +0900

    Change to use androidx library classes.

--- android/src/main/java/com/proyecto26/inappbrowser/ChromeTabsManagerActivity.java
+++ android/src/main/java/com/proyecto26/inappbrowser/ChromeTabsManagerActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import org.greenrobot.eventbus.EventBus;
 
